1b5d726f2e487216fa4a75f15a20d8eff4f54d62
redkrayn <redkrayn@yandex.ru>
2025-10-28 02:35:50 +0800

    Add related_name

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -233,7 +233,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
-        verbose_name='товар'
+        verbose_name='товар',
+        related_name='order_items'
     )
     quantity = models.PositiveIntegerField(
         default=1,
