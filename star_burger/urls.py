1e6f02bd6b8636396463895205cb75f45d1b8195
redkrayn <redkrayn@yandex.ru>
2025-11-03 23:54:19 +0800

    small fix

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -24,7 +24,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', render, kwargs={'template_name': 'index.html'}, name='start_page'),
-    path('', include('foodcartapp.urls')),
+    path('api/', include('foodcartapp.urls')),
     path('manager/', include('restaurateur.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
